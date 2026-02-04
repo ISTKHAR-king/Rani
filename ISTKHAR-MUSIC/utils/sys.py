7499d4027617f367983a7a3484cc5ded4f3537c2
ISTKHAR-king <istkharalam225@gmail.com>
2026-02-05 00:24:26 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ ISTKHAR-MUSIC/utils/sys.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------
+# 🔸 SUKKUMusic Project
+# 🔹 Developed & Maintained by: THUNDER ISTKHAR (https://github.com/itzISTKHAR)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzISTKHAR
+# -----------------------------------------------
+import time
+import psutil
+from ISTKHAR-MUSIC.misc import _boot_
+from ISTKHAR-MUSIC.utils.formatters import get_readable_time
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - _boot_)
+    UP = f"{get_readable_time(bot_uptime)}"
+    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
+    RAM = f"{psutil.virtual_memory().percent}%"
+    DISK = f"{psutil.disk_usage('/').percent}%"
+    return UP, CPU, RAM, DISK
