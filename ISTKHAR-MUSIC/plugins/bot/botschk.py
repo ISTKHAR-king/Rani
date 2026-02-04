c33ce6e96afddafe35a60dbb056b64356e3103f3
ISTKHAR-king <istkharalam225@gmail.com>
2026-02-05 00:21:26 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ ISTKHAR-MUSIC/plugins/bot/botschk.py
@@ -0,0 +1,60 @@
+# -----------------------------------------------
+# 🔸 SUKKUMusic Project
+# 🔹 Developed & Maintained by: THUNDER ISTKHAR (https://github.com/itzISTKHAR)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzISTKHAR
+# -----------------------------------------------
+import random
+from pyrogram import filters
+from ISTKHAR-MUSIC import app
+from ISTKHAR-MUSIC import *
+from ... import *
+import config
+from ...logging import LOGGER
+from ISTKHAR-MUSIC import app, userbot
+from ISTKHAR-MUSIC.core.userbot import *
+import asyncio
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import OWNER_ID
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from dotenv import load_dotenv
+from ISTKHAR-MUSIC.core.userbot import Userbot
+from datetime import datetime
+
+# Assuming Userbot is defined elsewhere
+userbot = Userbot()
+
+
+BOT_LIST = ["SUKKUSuperbot", "zmnrobot", "SapnaMusicRobot", "ITZ_MERADHIKABOT", "SUKKUHackBot"]
+
+@app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
+async def bots_chk(_, message):
+    msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
+    response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
+    for bot_username in BOT_LIST:
+        try:
+            bot = await app.get_users(bot_username)
+            bot_id = bot.id
+            await asyncio.sleep(0.5)
+            bot_info = await app.send_message(bot_id, "/start")
+            await asyncio.sleep(3)
+            async for bot_message in app.get_chat_history(bot_id, limit=1):
+                if bot_message.from_user.id == bot_id:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
+                else:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
+        except Exception:
+            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
+    
+    await msg.edit_text(response)
+                
